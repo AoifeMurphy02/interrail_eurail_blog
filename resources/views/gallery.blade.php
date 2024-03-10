c7e858d7bb6915dd35da430c5b20b386d168b1e8
AoifeMurphy02 <aoifemurphy2019@gmail.com>
2024-03-10 23:52:04 +0000

    added to the gallery page

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -29,7 +29,7 @@
           <img
             alt="gallery"
             class="block h-full w-full rounded-lg object-cover object-center"
-            src="https://c.files.bbci.co.uk/4D77/production/_102313891_teenspacksgetty19jul10berlin.jpg" />
+            src="https://www.euroventure.com/wp-content/uploads/2020/12/berlin-csp-16-600x533.jpg" />
         </div>
         <div class="w-1/2 p-1 md:p-2">
           <img
@@ -45,12 +45,56 @@
         </div>
       </div>
     </div>
-  </div>
-        <div>
+    
+       
+      </div>
+    </div>
+    <div class="container mx-auto px-5 py-2 lg:px-32 lg:pt-24">
+        <div class="-m-1 flex flex-wrap md:-m-2">
+          <div class="flex w-1/2 flex-wrap">
+            <div class="w-1/2 p-1 md:p-2">
+              <img
+                alt="gallery"
+                class="block h-full w-full rounded-lg object-cover object-center"
+                src="https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/ie7e383f97f22a8dd/version/1482760101/best-destinations-to-visit-by-train-stockholm-gamla-stan-the-old-part-of-stockholm-sweden-in-a-summer-day-copyright-c-european-best-destinations.jpg" />
+            </div>
+            <div class="w-1/2 p-1 md:p-2">
+              <img
+                alt="gallery"
+                class="block h-full w-full rounded-lg object-cover object-center"
+                src="https://d1bvpoagx8hqbg.cloudfront.net/originals/my-interrail-route-an-intense-trip-across-europe-469b8e41315ad9ff691bbf400d375943.jpg" />
+            </div>
+            <div class="w-full p-1 md:p-2">
+              <img
+                alt="gallery"
+                class="block h-full w-full rounded-lg object-cover object-center"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSN37JbjQLB439B3ueYMtd0Fj3zxLusxfp7rg&usqp=CAU" />
+            </div>
+          </div>
+          <div class="flex w-1/2 flex-wrap">
+            <div class="w-full p-1 md:p-2">
+              <img
+                alt="gallery"
+                class="block h-full w-full rounded-lg object-cover object-center"
+                src="https://c.files.bbci.co.uk/4D77/production/_102313891_teenspacksgetty19jul10berlin.jpg" />
+            </div>
+            <div class="w-1/2 p-1 md:p-2">
+              <img
+                alt="gallery"
+                class="block h-full w-full rounded-lg object-cover object-center"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOP0KJpLVJ4v3NS-w87OpJyxT77VnFrFnQkdHhUJl22rg8QLW0WYC1dVb99gR4-FBojPE&usqp=CAU" />
+            </div>
+            <div class="w-1/2 p-1 md:p-2">
+              <img
+                alt="gallery"
+                class="block h-full w-full rounded-lg object-cover object-center"
+                src="https://i.ytimg.com/vi/aMAwpLiq8YA/maxresdefault.jpg" />
+            </div>
+ 
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-        </head>
-        <body>
+       
+      
           
         <div class="container">
           
