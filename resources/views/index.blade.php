65f1736afc35e38be7aaefc69f27c1b08efc330e
AoifeMurphy02 <aoifemurphy2019@gmail.com>
2024-03-15 20:36:52 +0000

    Added css for responsive

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <div class="background-image grid grid-cols-1 m-auto">
+<div class="background-image grid grid-cols-1 m-auto">
         <div class="flex text-gray-100 pt-10">
             <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
                 <h1 class="sm:text-5xl uppercase font-bold pb-14 text-orange-300 drop-shadow-md" style="filter: drop-shadow(0 0 10px rgba(8, 8, 8, 0.8));">
@@ -19,38 +19,30 @@
     </div>
     <div class="my-8"></div>
 
-    <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/PRACK9_A5AE?si=B00G-KiLmFuI1Goj" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <div class="sm:grid grid-cols-1 md:grid-cols-2 gap-8 w-full md:w-4/5 mx-auto py-10 border-b border-gray-200">
+        <iframe class="mx-auto md:mx-0" width="100%" height="315" src="https://www.youtube.com/embed/PRACK9_A5AE?si=B00G-KiLmFuI1Goj" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       
-        <div class="m-auto sm:m-auto text-left w-4/5 block">
-            <h2 class="text-3xl font-extrabold ">
+        <div class="m-auto text-left w-full">
+            <h2 class="text-2xl font-extrabold pb-4">
                 Thinking of Exploring Europe by train?
             </h2>
             
-            <p class="py-8  text-s">
-                Memories to Last a Lifetime: InterRail isn't just a journey—it's a collection of moments that will stay with you long after 
-                you've returned home. Whether it's watching the sunset over the Mediterranean, 
-                stumbling upon a hidden gem in a picturesque village, or simply sharing a laugh with newfound friends, 
-                every experience is a cherished memory in the making.
+            <p class="py-2 text-sm">
+                Memories to Last a Lifetime: InterRail isn't just a journey—it's a collection of moments that will stay with you long after you've returned home.
             </p>
-            <p class="py-8  text-s">
-                InterRail isn't just about the destinations—it's about the journey. 
-                Along the way, you'll meet fellow travelers from all walks of life,
-                 each with their own stories to share. 
-                 Whether you're swapping travel tips over a meal or marveling at the passing scenery together, 
-                 the connections you make on the train are part of what makes the InterRail experience so special.
+            <p class="py-2 text-sm">
+                InterRail isn't just about the destinations—it's about the journey. Along the way, you'll meet fellow travelers from all walks of life, each with their own stories to share.
             </p>
-            <div class="my-8"></div>
             <a 
                 href="/blog"
-                class="uppercase  bg-orange-300 text-teal-700 text-s font-extrabold py-3 px-8 rounded-3xl">
+                class="uppercase bg-orange-300 text-teal-700 text-xs font-extrabold py-2 px-4 rounded-3xl inline-block mt-4">
                 Find Out More
             </a>
         </div>
     </div>
     <div class="my-8"></div>
 
-    <div class="text-center py-15">
+    <div class="text-center py-10">
         <h2 class="text-4xl font-bold py-10">
             Recent Posts
         </h2>
